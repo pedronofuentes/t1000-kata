43c4abd44ff3d9cb278360acfee8ee1632985b68
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2013-09-28 21:04:01 +0000

    red adding new target to T1000

--- tests/T1000/Tests/T1000Test.php
+++ tests/T1000/Tests/T1000Test.php
@@ -7,12 +7,25 @@ namespace T1000\Tests;
 use T1000\T1000;
 
 class T1000Test extends \PHPUnit_Framework_TestCase {
-    public function testT1000CreatedWithPosition() {
+
+    public function testT1000CreatedWithPosition()
+    {
         $aPosition = new \stdClass();
         $aT1000 = new T1000($aPosition);
 
         $position = $aT1000->getPosition();
 
         $this->assertSame($position, $aPosition);
+    }
+
+    public function testAddNewTargetToT1000()
+    {
+        $aPosition = new \stdClass();
+        $aT1000 = new T1000($aPosition);
+
+        $targetPosition = new \stdClass();
+        $aT1000->newTarget($targetPosition);
+
+        $this->assertEquals(1, count($aT1000->getTargets()));
     }
 } 
