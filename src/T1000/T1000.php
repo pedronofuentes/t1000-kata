0dc88f26043ff0ab0991ce229fab40b98d853320
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2013-09-28 21:10:39 +0000

    green added newTarget method

--- src/T1000/T1000.php
+++ src/T1000/T1000.php
@@ -7,13 +7,25 @@ namespace T1000;
 class T1000 {
 
     private $position;
+    private $targets;
 
     function __construct($position)
     {
         $this->position =  $position;
+        $this->targets = array();
     }
 
     public function getPosition() {
         return $this->position;
+    }
+
+    public function newTarget($targetPosition)
+    {
+        $this->targets[] = $targetPosition;
+    }
+
+    public function getTargets()
+    {
+        return $this->targets;
     }
 } 
