0ae76a30c968bff17b5b4a3589e8d568a637fddc
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2013-09-29 19:36:10 +0000

    red added test for next target return

--- tests/T1000/Tests/RoutingPattern/ChronologicalRoutingPatternTest.php
+++ tests/T1000/Tests/RoutingPattern/ChronologicalRoutingPatternTest.php
@@ -16,4 +16,18 @@ class ChronologicalRoutingPatternTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(1, count($chronologicalRoutingPattern->getTargets()));
     }
-} 
+
+    public function testChronologicalRoutingPatternHasToReturnFirstTargetIn()
+    {
+        $chronologicalRoutingPattern = new ChronologicalRoutingPattern();
+        $targetPositionA = new \stdClass(array(1, 3));
+        $targetPositionB = new \stdClass(array(2, 3));
+        $targetPositionC = new \stdClass(array(3, 3));
+
+        $chronologicalRoutingPattern->addNewTarget($targetPositionA);
+        $chronologicalRoutingPattern->addNewTarget($targetPositionB);
+        $chronologicalRoutingPattern->addNewTarget($targetPositionC);
+
+        $this->assertEquals($targetPositionA, $chronologicalRoutingPattern->getNextTarget());
+    }
+}
