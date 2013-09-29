8fa2aa8171d4f54f78fa8374aaa338f4e476ce3a
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2013-09-29 17:06:14 +0200

    red testing lost target method

--- tests/T1000/Tests/T1000Test.php
+++ tests/T1000/Tests/T1000Test.php
@@ -8,6 +8,18 @@ use T1000\T1000;
 
 class T1000Test extends \PHPUnit_Framework_TestCase {
 
+
+    /**
+     * @var T1000
+     */
+    private $t1000;
+
+    public function setUp()
+    {
+        $aPosition = new \stdClass();
+        $this->t1000 = new T1000($aPosition);
+    }
+
     public function testT1000CreatedWithPosition()
     {
         $aPosition = new \stdClass();
@@ -28,4 +40,16 @@ class T1000Test extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals(1, count($aT1000->getTargets()));
     }
-} 
+
+    public function testSendLostTargetSignalToT1000()
+    {
+        $targetPosition = new \stdClass(array(1, 2));
+        $anotherTargetPosition = new \stdClass(array(2, 3));
+        $this->t1000->newTarget($targetPosition);
+        $this->t1000->newTarget($anotherTargetPosition);
+
+        $this->t1000->lostTarget($targetPosition);
+
+        $this->assertEquals(1, count($this->t1000->getTargets()));
+    }
+}
