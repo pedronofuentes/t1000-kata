8bd9983a828362c087149b458ee8d6d242db0939
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2013-10-26 15:25:04 +0000

    red added test for call a callable when event is triggered

--- tests/T1000/Tests/EventDispatcher/EventDispatcherTest.php
+++ tests/T1000/Tests/EventDispatcher/EventDispatcherTest.php
@@ -4,8 +4,17 @@ namespace T1000\Tests\EventDispatcher;
 
 
 use \Mockery as m;
+use T1000\EventDispatcher\EventDispatcher;
 
 class EventDispatcherTest extends \PHPUnit_Framework_TestCase
 {
+    public function testItCallsCallableWhenAnEventIsTriggered()
+    {
+        $eventDispatcher = new EventDispatcher();
+        $functionMock = $this->getMock('AClass', array('aFunction'));
+        $functionMock->expects($this->once())->method('aFunction');
 
+        $eventDispatcher->attachEvent('EVENT', array($functionMock, 'aFunction'));
+        $eventDispatcher->trigger('EVENT');
+    }
 }
