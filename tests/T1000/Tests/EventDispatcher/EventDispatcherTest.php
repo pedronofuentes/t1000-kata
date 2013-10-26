565f9d0199d810d876446ea311bb678570c595a2
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2013-10-26 15:34:54 +0000

    red added test for handle multiple callbacks attached to one event

--- tests/T1000/Tests/EventDispatcher/EventDispatcherTest.php
+++ tests/T1000/Tests/EventDispatcher/EventDispatcherTest.php
@@ -3,18 +3,35 @@
 namespace T1000\Tests\EventDispatcher;
 
 
-use \Mockery as m;
 use T1000\EventDispatcher\EventDispatcher;
 
 class EventDispatcherTest extends \PHPUnit_Framework_TestCase
 {
+    private $eventDispatcher;
+
+    public function setUp()
+    {
+        $this->eventDispatcher = new EventDispatcher();
+    }
+
     public function testItCallsCallableWhenAnEventIsTriggered()
     {
-        $eventDispatcher = new EventDispatcher();
         $functionMock = $this->getMock('AClass', array('aFunction'));
         $functionMock->expects($this->once())->method('aFunction');
 
-        $eventDispatcher->attachEvent('EVENT', array($functionMock, 'aFunction'));
-        $eventDispatcher->trigger('EVENT');
+        $this->eventDispatcher->attachEvent('EVENT', array($functionMock, 'aFunction'));
+        $this->eventDispatcher->trigger('EVENT');
+    }
+
+    public function testItHandlesMultipleCallablesAttachedToAnEvent()
+    {
+        $functionMock = $this->getMock('AClass', array('aFunction', 'anotherFunction'));
+        $functionMock->expects($this->once())->method('aFunction');
+        $functionMock->expects($this->once())->method('anotherFunction');
+
+        $this->eventDispatcher->attachEvent('EVENT', array($functionMock, 'aFunction'));
+        $this->eventDispatcher->attachEvent('EVENT', array($functionMock, 'anotherFunction'));
+
+        $this->eventDispatcher->trigger('EVENT');
     }
 }
